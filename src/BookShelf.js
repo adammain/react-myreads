2c0c4b4fda6ed9afbc40d6b939f52cb145338521
amtruenorth <northforth@me.com>
2017-09-11 15:10:35 -0500

    refactor: Update for book component

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react"
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types"
 import { Route, Link } from "react-router-dom"
+import Book from "./Book"
 
 class BookShelf extends Component {
   static propTypes = {
@@ -8,57 +9,38 @@ class BookShelf extends Component {
     books: PropTypes.array
   }
 
+  getShelfTitle(shelf) {
+    console.log(shelf)
+    if (shelf === "currentlyReading")
+      return "Currently Reading"
+    else if (shelf === "wantToRead")
+      return "Want to Read"
+    else if (shelf === "read")
+      return "Read"
+    else
+      return "Unknown"
+  }
+
+  renderBook() {
+    return (
+      this.props.books.map((book, index) => (
+        <li key={index}>
+          <Book book={book} />
+        </li>
+      ))
+    )
+  }
+
   render() {
-    console.log(this.props.books, this.props.shelf)
+    // console.log(this.props.books, this.props.shelf)
     return (
       <div className="bookshelf">
         <h2 className="bookshelf-title">
-          {
-            this.props.shelf
-              ===  "currentlyReading" ? "Currently Reading"
-              : "wantToRead" ? "Want to Read"
-              : "read" ? "Read"
-              : "unknown"
-          }
+          { this.getShelfTitle(this.props.shelf) }
       </h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
-            <li>
-              <div className="book">
-                <div className="book-top">
-                  <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
-                  <div className="book-shelf-changer">
-                    <select>
-                      <option value="none" disabled>Move to...</option>
-                      <option value="currentlyReading">Currently Reading</option>
-                      <option value="wantToRead">Want to Read</option>
-                      <option value="read">Read</option>
-                      <option value="none">None</option>
-                    </select>
-                  </div>
-                </div>
-                <div className="book-title">To Kill a Mockingbird</div>
-                <div className="book-authors">Harper Lee</div>
-              </div>
-            </li>
-            <li>
-              <div className="book">
-                <div className="book-top">
-                  <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: 'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")' }}></div>
-                  <div className="book-shelf-changer">
-                    <select>
-                      <option value="none" disabled>Move to...</option>
-                      <option value="currentlyReading">Currently Reading</option>
-                      <option value="wantToRead">Want to Read</option>
-                      <option value="read">Read</option>
-                      <option value="none">None</option>
-                    </select>
-                  </div>
-                </div>
-                <div className="book-title">Ender's Game</div>
-                <div className="book-authors">Orson Scott Card</div>
-              </div>
-            </li>
+            { this.renderBook() }
           </ol>
         </div>
       </div>
